dbbb438054ec7b664c524cc44eac2a5c16438cc8
WarrenT <warrentyler1024@gmail.com>
2024-02-04 20:54:35 +0000

    Implement main App component
    - Include Navbar, Sidebar, Feed, and Rightbar components
    - Use Stack for layout with horizontal direction
    - Set spacing and background color for styling
    - Commented-out code for potential future use

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import Typography from "@mui/material/Typography";
+// import { Box, Divider, Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Typography>App</Typography>
-    </div>
+    <Box>
+    <Navbar />
+      <Stack
+        direction="row"
+        // divider={<Divider orientation="vertical" flexItem />}
+        spacing={2}
+        // justifyContent="space-between"
+        sx={{ backgroundColor: "#ddd" }}
+      >
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
-}
+};
 export default App;

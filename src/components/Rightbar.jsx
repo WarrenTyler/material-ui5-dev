6df285bf46f8a8851a821e2fa95bd2f83a144d00
WarrenT <warrentyler1024@gmail.com>
2024-02-04 20:51:26 +0000

    Implement Rightbar component
    - Add Rightbar component with Box layout
    - Set flex, padding, and responsive display
    - Set background color for styling

---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material"
+
+const Rightbar = () => {
+  return (
+    <Box
+      flex={2}
+      p={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+      bgcolor={"#dd88dd"}
+    >
+      Rightbar
+    </Box>
+  );
+}
+export default Rightbar

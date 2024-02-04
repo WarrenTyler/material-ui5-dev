da7bc8b2855beb1542f509bab3db88dd93a4e463
WarrenT <warrentyler1024@gmail.com>
2024-02-04 20:53:58 +0000

    Implement Feed component
    - Add Feed component with Box layout
    - Set flex, padding, and background color for styling

---
+++ src/components/Feed.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material"
+
+const Feed = () => {
+  return (
+    <Box
+      flex={4}
+      p={2}
+      bgcolor={"#339999"}
+    >
+      Feed
+    </Box>
+  );
+}
+export default Feed

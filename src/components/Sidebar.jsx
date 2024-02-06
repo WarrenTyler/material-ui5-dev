af55005df516fad6ab03203482d8cf0da1cc59bf
WarrenT <warrentyler1024@gmail.com>
2024-02-06 06:21:14 +0000

    Create navigation Sidebar with icons and links design
    - Integrate MUI icons for navigation items
    - Create a responsive Sidebar component with list items
    - Add links and corresponding icons for each item
    - Include a switch for night mode

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,95 @@
-import { Box } from "@mui/material";
+import {
+  AccountBox,
+  Article,
+  Group,
+  Home,
+  ModeNight,
+  Person,
+  Settings,
+  Storefront,
+} from "@mui/icons-material";
+
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
 
 const Sidebar = () => {
   return (
-    <Box
-      flex={1}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-      bgcolor={"#ee5544"}
-    >
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <Article />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <Group />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <Storefront />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <AccountBox />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <ModeNight />
+            </ListItemIcon>
+            <Switch
+              // onChange={(e) => setMode(mode === "light" ? "dark" : "light")}
+            />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
